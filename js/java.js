4c8c9999d28e2e71b2dd52b54f5b710ab2287b99
Jarom Oyler <bubba@Bubba.local>
2022-10-17 07:48:49 -0600

    I made the pars work

--- js/java.js
+++ js/java.js
@@ -1,18 +1,27 @@
- let api_golf = "https://golf-courses-api.herokuapp.com/courses/"
- let api_golf11819 = "https://golf-courses-api.herokuapp.com/courses/11819"
- let y = 1
- let a = 11819
+let api_golf = "https://golf-courses-api.herokuapp.com/courses/"
+
+let e = document.querySelector(".form-control")
+let y = 1
+let a = 0
+
 let golf_courses =[]
 let player1_score =[]
 let player2_score =[]
 let player3_score =[]
 let player4_score =[]
 
-fetch(api_golf11819)
-.then(response=> response.json())
-.then(result =>{
-    par11819(result.data)
+let bluring = document.querySelectorAll(".blur")
+bluring.forEach(bl =>{
+    bl.style.opacity ="0.5"
 })
+
+let bluring1 = document.querySelectorAll(".blur1")
+bluring1.forEach(bl1 =>{
+    bl1.style.opacity ="0.5"
+
+})
+
+document.querySelector(".blur").style.opacity = "0.5"
 
 
 fetch(api_golf)
@@ -22,26 +31,59 @@ fetch(api_golf)
     bob(golf_courses)
 })
 
-function par11819(data){
-    console.log(data)
+function par(data){
     let x = 0
+    let element = document.querySelectorAll(".blur")
+    element.forEach(el =>{
+        el.textContent = "# of Hits"
+    })
+
     if (a ==11819){
         for(i=1; i<19;i++){
             
-            document.querySelector(".par"+i).textContent = "par " + data.holes[x].changeLocations[0].par
+            document.querySelector(".par"+i).textContent = "par " + data.holes[x].teeBoxes[0].par
+
             x++
         }
     }
+    if (a ==19002){
+        for(i=1; i<19;i++){
+            
+            document.querySelector(".par"+i).textContent = "par " + data.holes[x].teeBoxes[0].par
+            
+            x++
+        }
+    }
+    
+    if (a ==18300){
+        for(i=1; i<19;i++){
+
+            document.querySelector(".par"+i).textContent = "par " + data.holes[x].teeBoxes[0].par
+            
+            x++
+        }
+    }
+    if (a == 0){
+        for(i=1; i<19;i++){
+
+            document.querySelector(".par"+i).textContent = "par "
+            
+            x++
+        }
+    }
+    
     
 }
 
 function bob(data){
     //section selector
     golf_courses= data
-   
+    
     for(i= 0; i<golf_courses.length; i++){
         let x = i+1
+        let select = document.querySelector('.golf'+x);
         document.querySelector(".golf"+x).textContent = golf_courses[i].name + " " + golf_courses[i].id
+        select.value = golf_courses[i].id
     }
     
 
@@ -62,7 +104,6 @@ function clickFunction(){
             return y
         }
         
-        console.log(y)
         if (adding.length > 10){
             document.querySelector(".player"+y).textContent = adding.slice(0,7)+"...";
         }else{
@@ -71,4 +112,21 @@ function clickFunction(){
         y =y+1
     }
 
+}
+
+function getInfo(){
+    let value = e.value;
+    let api_golf11819 = "https://golf-courses-api.herokuapp.com/courses/"+value
+    a = value
+    if( a == 0 ){
+        return par(0)
+    } else{
+        fetch(api_golf11819)
+        .then(response=> response.json())
+        .then(result =>{
+            par(result.data)
+            
+        })
+    }
+    
 }
