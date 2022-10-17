c5f24055642f001cb29cba1d3564b8598dbe5bcb
Jarom Oyler <bubba@Bubba.local>
2022-10-17 10:28:33 -0600

    You can Know add scores

--- js/java.js
+++ js/java.js
@@ -4,7 +4,10 @@ let e = document.querySelector(".form-control")
 let y = 1
 let a = 0
 
+
+let selected_golf = false;
 let golf_courses =[]
+let golf_par =[]
 let player1_score =[]
 let player2_score =[]
 let player3_score =[]
@@ -15,13 +18,12 @@ bluring.forEach(bl =>{
     bl.style.opacity ="0.5"
 })
 
-let bluring1 = document.querySelectorAll(".blur1")
-bluring1.forEach(bl1 =>{
-    bl1.style.opacity ="0.5"
-
-})
-
-document.querySelector(".blur").style.opacity = "0.5"
+for(i = 0 ; i<6 ; i++){
+    let bluring = document.querySelectorAll(".blur"+i)
+    bluring.forEach(bl =>{
+        bl.style.opacity ="0.5"
+    })
+}
 
 
 fetch(api_golf)
@@ -37,38 +39,47 @@ function par(data){
     element.forEach(el =>{
         el.textContent = "# of Hits"
     })
+    
 
     if (a ==11819){
+        selected_golf = true
         for(i=1; i<19;i++){
             
             document.querySelector(".par"+i).textContent = "par " + data.holes[x].teeBoxes[0].par
-
+            golf_par[i-1] =data.holes[x].teeBoxes[0].par
             x++
+            q=1
         }
     }
     if (a ==19002){
+        selected_golf = true
         for(i=1; i<19;i++){
             
             document.querySelector(".par"+i).textContent = "par " + data.holes[x].teeBoxes[0].par
-            
+            golf_par[i-1] =data.holes[x].teeBoxes[0].par
             x++
+            q=1
         }
     }
     
     if (a ==18300){
+        selected_golf = true
         for(i=1; i<19;i++){
 
             document.querySelector(".par"+i).textContent = "par " + data.holes[x].teeBoxes[0].par
-            
+            golf_par[i-1] =data.holes[x].teeBoxes[0].par
             x++
+            q=1
         }
     }
     if (a == 0){
+        selected_golf = false
         for(i=1; i<19;i++){
 
             document.querySelector(".par"+i).textContent = "par "
-            
+            golf_par[i-1] =data.holes[x].teeBoxes[0].par
             x++
+            q=1
         }
     }
     
@@ -93,7 +104,7 @@ function clickFunction(){
     //add player
     let adding = document.querySelector(".add").value
     var isEmpty = adding.length == 0;
-    
+    let nameblur = document.querySelector(".player"+y)
     if(isEmpty){
         alert("Add Player Name Is Empty")
     }else{
@@ -105,10 +116,23 @@ function clickFunction(){
         }
         
         if (adding.length > 10){
+            
+
             document.querySelector(".player"+y).textContent = adding.slice(0,7)+"...";
         }else{
+            
             document.querySelector(".player"+y).textContent = adding
         }
+        nameblur.style.opacity ="1"
+        let bluring = document.querySelectorAll(".blur"+y)
+        bluring.forEach(bl =>{
+            bl.style.opacity ="1"
+            bl.contentEditable = true
+        })
+
+        document.querySelector(".player"+y).contentEditable = true;
+        document.querySelector(".total").contentEditable = false;
+    
         y =y+1
     }
 
@@ -130,3 +154,66 @@ function getInfo(){
     }
     
 }
+
+
+let row_number
+let col_number
+function myFunction(x) {
+    row_number =x.rowIndex;
+    
+    
+}
+function myFunction1(x) {
+    col_number = x.cellIndex;
+    
+}
+
+
+let div = document.querySelectorAll(".blur")
+
+div.forEach(div =>{
+ 
+    
+    div.addEventListener("blur", ()=> {
+        
+        if( selected_golf == false){
+            alert("Plz Select a Golf Course")
+            div.textContent = "# of Hits"
+        
+        }
+        else if(Number.isInteger(Number(div.textContent)) == false){
+            div.textContent = "# of Hits"
+            alert("Plz Put in Whole Number!!!!!")
+            
+        }else{
+            let total = div.textContent - golf_par[row_number-1]
+            div.contentEditable = false;
+            if(total>= 0){
+                
+                div.textContent = "+"+total
+                if(col_number==1){
+                    player1_score[row_number-1] = total
+                }else if(col_number==2){
+                    player2_score[row_number-1] = total
+                }else if(col_number==3){
+                    player3_score[row_number-1] = total
+                }else if(col_number==4){
+                    player4_score[row_number-1] = total
+                }
+            }else{
+                div.textContent = total
+                if(col_number==1){
+                    player1_score[row_number-1] = total
+                }else if(col_number==2){
+                    player2_score[row_number-1] = total
+                }else if(col_number==3){
+                    player3_score[row_number-1] = total
+                }else if(col_number==4){
+                    player4_score[row_number-1] = total
+                }
+            }
+            
+            
+        }
+    })
+})
